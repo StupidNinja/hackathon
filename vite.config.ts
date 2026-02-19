8a3edd7887e906ab16996730248f3de314dd81f5
StupidNinja <temirkhan.adilet05@gmail.com>
2026-02-19 12:48:47 +0500

    chore(build): update deps and remove tanstack router tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tanstackRouter({
-      routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
-    }),
     tailwindcss(),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
